89b7ed755cc78d28dfa275ecdec9ffb6e1d86650
selimsahnoun <sahnounselim@gmail.com>
2020-11-04 13:55:47 +0100

    home page in progress doesnt render yet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
-  declarations: [AppComponent, NavBarComponent],
+  declarations: [AppComponent, NavBarComponent, HomePageComponent],
   imports: [BrowserModule, MDBBootstrapModule.forRoot()],
   providers: [],
   bootstrap: [AppComponent],
