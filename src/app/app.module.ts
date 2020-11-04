2a46b5b95071f5678e158a0b5376f75b22861555
selimsahnoun <sahnounselim@gmail.com>
2020-11-04 15:05:28 +0100

    the app renders login register nav bar homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,11 @@ import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
-  declarations: [AppComponent, NavBarComponent, HomePageComponent],
+  declarations: [AppComponent, NavBarComponent, HomePageComponent, LoginComponent, RegisterComponent],
   imports: [BrowserModule, MDBBootstrapModule.forRoot()],
   providers: [],
   bootstrap: [AppComponent],
