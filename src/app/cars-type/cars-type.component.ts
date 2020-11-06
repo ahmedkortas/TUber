6a085ef1ca9f16f105a48c436ce02bc1ab4fcf22
unknown <heni.oussama23@gmail.com>
2020-11-06 19:02:54 +0100

    created cars type component

---
+++ src/app/cars-type/cars-type.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cars-type',
+  templateUrl: './cars-type.component.html',
+  styleUrls: ['./cars-type.component.scss']
+})
+export class CarsTypeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
