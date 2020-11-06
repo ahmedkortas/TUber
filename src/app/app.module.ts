c4b248e3aff03aeda821390b6e7ad1f6372b2e96
Ahmed Kortas <42817639+ahmedkortas@users.noreply.github.com>
2020-11-06 11:24:41 +0100

    form init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { ResetComponentComponent } from './reset-component/reset-component.component';
 import { NgModule } from '@angular/core';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { Routes, RouterModule } from '@angular/router';
@@ -8,6 +9,7 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'; //import this in the app module !!!important
 
 const routes: Routes = [
   { path: '', component: HomePageComponent },
@@ -17,14 +19,17 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  declarations: [
+  declarations: [	
     AppComponent,
     NavBarComponent,
     HomePageComponent,
     LoginComponent,
     RegisterComponent,
-  ],
+      ResetComponentComponent
+   ],
   imports: [
+    FormsModule,
+    ReactiveFormsModule,
     BrowserModule,
     MDBBootstrapModule.forRoot(),
     RouterModule.forRoot(routes),
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -9,4 +10,8 @@ export class RegisterComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
+  onSubmit(f: NgForm) {
+    console.log(f.value); // { first: '', last: '' }
+    console.log(f.valid); // false
+  }
 }
